b4611f076f732b0e5cba58311d461d6c88644a3a
E.Barboza <e.barboza@edu.unipar.br>
2022-08-10 13:08:24 -0300

    Adicionando arquivos

--- procurando-na-lista.js
+++ procurando-na-lista.js
@@ -6,6 +6,7 @@ let listaDeNotasEAlunos = [alunos, medias];
 const exibeNomeNota = (nomeAluno) => {
     if (listaDeNotasEAlunos[0].includes(nomeAluno)){
         indice = listaDeNotasEAlunos[0].indexOf(nomeAluno);
+        // Novos comandos
         return listaDeNotasEAlunos[0][indice] + ', sua média é ' + listaDeNotasEAlunos[1][indice];
     } else {
         return 'Aluno não está cadastrado';
