9239e14323482dfb95d88545b0ad4c9a2689d41e
E.Barboza <e.barboza@edu.unipar.br>
2022-08-10 12:58:10 -0300

    Adicionando estudo 'procurando-na-lista.js'

---
+++ procurando-na-lista.js
@@ -0,0 +1,15 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio'];
+const medias = [10, 8, 7.5, 9];
+
+let listaDeNotasEAlunos = [alunos, medias];
+
+const exibeNomeNota = (nomeAluno) => {
+    if (listaDeNotasEAlunos[0].includes(nomeAluno)){
+        indice = listaDeNotasEAlunos[0].indexOf(nomeAluno);
+        return listaDeNotasEAlunos[0][indice] + ', sua média é ' + listaDeNotasEAlunos[1][indice];
+    } else {
+        return 'Aluno não está cadastrado';
+    }
+}
+
+console.log(exibeNomeNota('Emanuel'));
