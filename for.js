7c8159ca26747fa2dffb48ad7828d65d930ebc5b
E.Barboza <e.barboza@edu.unipar.br>
2022-08-15 08:40:26 -0300

    Adicionando estudos

--- for.js
+++ for.js
@@ -1,5 +1,9 @@
 const numeros = [100, 200, 300, 400, 500, 600];
 
 for (let i = 0; i < numeros.length; i++){
+// Criamos um laço com o comando for
+// Criamos uma variável de controle e atribuimos o valor 0
+// .length é um comando que "anda" pela variável
+// ++ é um comando que adiciona + 1 ao valor anterior da variável
     console.log(i, numeros[i]);
 }
